5cc18ae52cf60978503351f49fd91ede1518021d
Leilamica <163433407+Leilamica@users.noreply.github.com>
2024-10-08 14:56:06 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+
+namespace new_email_tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("FromMyNotes", "do-not-reply@frommynotes.com"));
+            message.To.Add(new MailboxAddress("Leilamica", "leilamicacenita@gmail.com"));
+            message.Subject = "Thanks for Subscribing!";
+
+            message.Body = new TextPart("html")
+            {
+                Text = "<h1>Hi, User!</h1>" +
+                "<p>Thank you for subscribing to frommynotes newsletter.</p>" +
+                "<p><strong>Welcome!</strong></p>"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect("sandbox.smtp.mailtrap.io", 2525, MailKit.Security.SecureSocketOptions.StartTls);
+
+                    client.Authenticate("f63b8c074499f3", "f70d91aca99cf7");
+
+                    client.Send(message);
+                    Console.WriteLine("Email sent successfully through Mailtrap.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error sending email: {ex.Message}");
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                }
+            }
+        }
+    }
+}
